faffabd1c77f3beed0d8f3cf8d0cbffe7183e032
Kamilla <113370998+Kanin60@users.noreply.github.com>
2023-02-28 13:40:00 +0100

    færdig

--- loop.js
+++ loop.js
@@ -1,5 +1,12 @@
-
-for (let i = 1; i < 11; i++) {
+/*OPGAVE 1
+Din opgave er at lave et for loop der tæller fra 1 til 72 i konsollen på din browser.*/
+for (let i = 1; i < 73; i++) {
     console.log(i);
 }
 
+
+/*OPGAVE 2
+Lav en countdown der tæller ned fra 5 til 1, denne gang skal hvert tal vises i en alert i browseren.*/
+for (let index = 5; index >0; index--) {
+    alert(index);
+};
